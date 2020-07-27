6e05c5869daef7bc8103d9740c16c335049dcda3
WikKam <kamykikamienie@gmail.com>
2020-07-27 15:08:46 +0200

    Updates

---
+++ precache-manifest.510ced10d4b3013c1bafceb0c9075ead.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4011895275d1432c8e48c0421eeba162",
+    "url": "/SpotifyCollageFront/index.html"
+  },
+  {
+    "revision": "0f60cf389d4037755946",
+    "url": "/SpotifyCollageFront/static/css/main.63cdc941.chunk.css"
+  },
+  {
+    "revision": "03f9b8d84d10fb8b30e4",
+    "url": "/SpotifyCollageFront/static/js/2.356929a7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/SpotifyCollageFront/static/js/2.356929a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f60cf389d4037755946",
+    "url": "/SpotifyCollageFront/static/js/main.1bad5a17.chunk.js"
+  },
+  {
+    "revision": "aedb80ef1b56873be85f",
+    "url": "/SpotifyCollageFront/static/js/runtime-main.e91e4b4e.js"
+  },
+  {
+    "revision": "11dc437ab8ef0c32289c092083d20068",
+    "url": "/SpotifyCollageFront/static/media/Spotify_Logo_RGB_Green.11dc437a.png"
+  },
+  {
+    "revision": "022af2f62a7c0af68de51fa2cbf218e2",
+    "url": "/SpotifyCollageFront/static/media/iCiel Gotham-Medium.022af2f6.ttf"
+  }
+]);
