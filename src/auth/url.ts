76be929875615d2e2c61f22a388d2fc35a1100af
Wiktor Kamiński <wkaminski@qualtrics.com>
2022-08-07 23:23:46 +0200

    moved clientId to env

--- src/auth/url.ts
+++ src/auth/url.ts
@@ -1,4 +1,4 @@
 export const oauthEndpoint = "https://accounts.spotify.com/authorize";
-export const clientId = "e34dc5d417494fb6be8b663a9631b502";
+export const clientId = import.meta.env.VITE_CLIENT_ID;
 export const redirectUri = window.location.origin;
 export const scopes = "user-read-private user-read-email playlist-read-private playlist-read-collaborative user-read-recently-played user-top-read"
